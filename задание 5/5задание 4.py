72fb5ae30e4e71baba1ac6f9877f38f711249d96
LizaBakalina <144846481+LizaBakalina@users.noreply.github.com>
2023-10-31 12:44:58 +0300

    Update 5задание 4.py

--- задание 5/5задание 4.py
+++ задание 5/5задание 4.py
@@ -1,9 +1,9 @@
-x = int(input())
-y = int(input())
-def F():
+a = int(input())
+b = int(input())
+def F(x,y):
     i = 1
     while x < y:
         x = x * 1.1
         i += 1
         print(i)
-F()
+F(a,b)
