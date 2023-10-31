2976c3e7ccac9e27160b75731dcabe68b5162e5d
LizaBakalina <144846481+LizaBakalina@users.noreply.github.com>
2023-10-31 23:05:01 +0300

    Update 7задание 1.py

--- 7 задание 2 вар/7задание 1.py
+++ 7 задание 2 вар/7задание 1.py
@@ -1,6 +1,8 @@
-x = int(input())
-a = []
-for i in range(x):
-    a.append(int(input()))
-print(a)
-print(min(a))
+def F():
+    x = int(input())
+    a = []
+    for i in range(x):
+        a.append(int(input()))
+    print(a)
+    print(min(a))
+F()
