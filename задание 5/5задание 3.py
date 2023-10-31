10d8a32e3a0368054a5ef8f68eaf56056124168a
LizaBakalina <144846481+LizaBakalina@users.noreply.github.com>
2023-10-31 12:39:31 +0300

    Update 5задание 3.py

--- задание 5/5задание 3.py
+++ задание 5/5задание 3.py
@@ -1,7 +1,9 @@
 N= int(input())
-i = 2
-c = 1
-while i <=N:
-    i*=2
-    c +=1
-    print(c-1,i//2)
+def F():
+    i = 2
+    c = 1
+    while i <=N:
+        i*=2
+        c +=1
+        print(c-1,i//2)
+F()
