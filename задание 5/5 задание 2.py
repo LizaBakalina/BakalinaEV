9ce0ea1fdfac302b57f3ca47805e5b85271a93bc
LizaBakalina <144846481+LizaBakalina@users.noreply.github.com>
2023-10-31 12:37:52 +0300

    Update 5 задание 2.py

--- задание 5/5 задание 2.py
+++ задание 5/5 задание 2.py
@@ -1,7 +1,9 @@
 i = int(input())
-a=1
-while a<=i:
-    a +=1
-    if i%a == 0:
-        print(i)
-        break
+def F():
+    a=1
+    while a<=i:
+        a +=1
+        if i%a == 0:
+            print(i)
+            break
+F()
