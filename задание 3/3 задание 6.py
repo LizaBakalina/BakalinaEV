9a8168e03202427142e79e5cf791129a90d601aa
LizaBakalina <144846481+LizaBakalina@users.noreply.github.com>
2023-10-31 09:10:51 +0300

    Update 3 задание 6.py

--- задание 3/3 задание 6.py
+++ задание 3/3 задание 6.py
@@ -1,8 +1,13 @@
-a = int(input())#номер столбца 1 кл
-b = int(input())#номер строки 1 кл
-c = int(input())#номер столбца 2 кл
-d = int(input())#номер строки 2 кл
-if ((a%2 ==1 and b%2 == 1) or (a%2 == 0 and b%2 == 0)) and ((c%2 ==1 and d%2 == 1) or (c%2 == 0 and d%2 == 0)) or ((a%2 == 1 and b%2 == 0) or (a%2 == 0 and b%2 == 1)) and ((c%2 == 1 and d%2 == 0) or (c%2 == 0 and d%2 == 1)):
-    print('Да')
-else:
-    print('Нет')
+x = int(input())#номер столбца 1 кл
+y = int(input())#номер строки 1 кл
+z = int(input())#номер столбца 2 кл
+g = int(input())#номер строки 2 кл
+def F(a,b,c,d):
+    if ((a % 2 == 1 and b % 2 == 1) or (a % 2 == 0 and b % 2 == 0)) and (
+            (c % 2 == 1 and d % 2 == 1) or (c % 2 == 0 and d % 2 == 0)) or (
+            (a % 2 == 1 and b % 2 == 0) or (a % 2 == 0 and b % 2 == 1)) and (
+            (c % 2 == 1 and d % 2 == 0) or (c % 2 == 0 and d % 2 == 1)):
+        print('Да')
+    else:
+        print('Нет')
+F(x,y,z,g)
