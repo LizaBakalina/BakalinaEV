044ec0c2e3e636ec023db4773f3078d23f1ae5f0
LizaBakalina <144846481+LizaBakalina@users.noreply.github.com>
2023-10-31 12:50:44 +0300

    Update 5задание 7.py

--- задание 5/5задание 7.py
+++ задание 5/5задание 7.py
@@ -1,8 +1,10 @@
-a = int(input())
-b = 0
-while a != 0:
-    n = int(input())
-    if n != 0 and a < n:
-        b += 1
-    a = n
-print(b)
+def F():
+    a = int(input())
+    b = 0
+    while a != 0:
+        n = int(input())
+        if n != 0 and a < n:
+            b += 1
+        a = n
+    print(b)
+F()
