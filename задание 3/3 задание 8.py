d0bacd6515fea93a973b2de3276e5b48d8fc267b
LizaBakalina <144846481+LizaBakalina@users.noreply.github.com>
2023-10-31 09:16:55 +0300

    Update 3 задание 8.py

--- задание 3/3 задание 8.py
+++ задание 3/3 задание 8.py
@@ -1,9 +1,11 @@
 a = int(input())
 b = int(input())
 c = int(input())
-if a == b ==c:
-    print(3)
-if a == b or b ==c or a == c:
-    print(2)
-else:
-    print(0)
+def F(x,y,z):
+    if x == y ==z:
+        print(3)
+    if x == y or y == z or z == x:
+        print(2)
+    else:
+        print(0)
+F(a,b,c)
