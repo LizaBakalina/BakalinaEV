770c2cfd83bc3f427e3511bd612e4c0d5e41e7b4
LizaBakalina <144846481+LizaBakalina@users.noreply.github.com>
2023-10-31 12:25:32 +0300

    Update 4 задание 7.py

--- задание 4/4 задание 7.py
+++ задание 4/4 задание 7.py
@@ -1,22 +1,24 @@
 N = int(input())
 K = int(input())
-fib1 = 1
-fib2 = 1
-f = 0
-kol = 0
-c = 0
-A = []
-B = []
-while c < N:
-    kol += 1
-    if len(A) <= 1:
-        A.append(1)
-    else:
-        f = fib1 + fib2
-        fib1 = fib2
-        fib2 = f
-        A.append(f)
-    if kol >= K:
-        B.append(A[-1])
-        c += 1
-print(sum(B))
+def F():
+    fib1 = 1
+    fib2 = 1
+    f = 0
+    kol = 0
+    c = 0
+    A = []
+    B = []
+    while c < N:
+        kol += 1
+        if len(A) <= 1:
+            A.append(1)
+        else:
+            f = fib1 + fib2
+            fib1 = fib2
+            fib2 = f
+            A.append(f)
+        if kol >= K:
+            B.append(A[-1])
+            c += 1
+    print(sum(B))
+F()
