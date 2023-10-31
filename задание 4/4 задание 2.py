c6dff40b5feb71b231c0947edf290d1aa5287d0a
LizaBakalina <144846481+LizaBakalina@users.noreply.github.com>
2023-10-31 12:09:01 +0300

    Update 4 задание 2.py

--- задание 4/4 задание 2.py
+++ задание 4/4 задание 2.py
@@ -1,11 +1,13 @@
 A = int (input())
 B = int (input())
-x = []
-if A < B:
-    for i in range(A, B + 1):
-        x.append(i)
-else:
-    for j in range(B, A + 1):
-        x.append(j)
-    x.reverse()
-print(x)
+def F():
+    x = []
+    if A < B:
+        for i in range(A, B + 1):
+            x.append(i)
+    else:
+        for j in range(B, A + 1):
+            x.append(j)
+        x.reverse()
+    print(x)
+F()
