ac353f6edd990f113943dcca8ecac0d0401612bc
LizaBakalina <144846481+LizaBakalina@users.noreply.github.com>
2023-10-31 12:32:05 +0300

    Update 4 задание 9.py

--- задание 4/4 задание 9.py
+++ задание 4/4 задание 9.py
@@ -1,6 +1,8 @@
-a = b =1 #первый и второй элемент
 N = int(input())-2
-print(a,b,end = ' ')
-for i in range(2,N):
-    a,b = b, a +b
-    print(b,end = ' ')
+def F():
+    a = b = 1
+    print(a,b,end = ' ')
+    for i in range(2,N):
+        a,b = b, a +b
+        print(b,end = ' ')
+F()
