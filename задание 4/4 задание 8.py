80e292ad09784d9c1e5b57d2dd5a6c682bde0f35
LizaBakalina <144846481+LizaBakalina@users.noreply.github.com>
2023-10-31 12:27:06 +0300

    Update 4 задание 8.py

--- задание 4/4 задание 8.py
+++ задание 4/4 задание 8.py
@@ -1,6 +1,8 @@
 n = int(input())
-if n <=9:
-    for i in range(n):
-        for j in range(1, i+1):
-            print(j , sep = '', end = '' )
-        print()
+def F():
+    if n <=9:
+        for i in range(n):
+            for j in range(1, i+1):
+                print(j , sep = '', end = '' )
+            print()
+F()
