9aef88e57cb286333b3bcc6f6c72707ade30d9c2
LizaBakalina <144846481+LizaBakalina@users.noreply.github.com>
2024-02-27 10:24:20 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,13 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 import java.util.Random;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        /*File TextFile = new File("example.txt");
+        FileWriter fileWriter = new FileWriter(TextFile);*/
         Scanner in = new Scanner(System.in);
         System.out.print("Ввод размера квадратной матрицы: ");
         int n = in.nextInt();
@@ -24,45 +29,34 @@ public class Main {
             sb.append("\n");
         }
 
-        System.out.println(sb);
+        /*System.out.println(sb);
+        fileWriter.write(sb.toString());
+        fileWriter.close();*/
 
         int diag1 = 1;
         int diag2 = 1;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i == j){
-                    diag1 = diag1*matrix[i][j];
-                }if (i + j == n-1) {
-                    diag2 = diag2 * matrix[i][j];
-                }
-
-            }
-
-        }
-        System.out.println("Произведение диагоналей: " + diag1*diag2);
-
         int sum = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i + j < n-1){
-                    sum = sum+matrix[i][j];
-                }
-
-            }
-
-        }
-        System.out.println("Сумма элементов матрицы выше диагонали: " + sum);
-
         int raz = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if (i + j > n-1){
-                    raz = raz + (-1)*matrix[i][j];
+                if (i == j) {
+                    diag1 = diag1 * matrix[i][j];
+                }
+                if (i + j == n - 1) {
+                    diag2 = diag2 * matrix[i][j];
+                }
+                if (i + j < n - 1) {
+                    sum = sum + matrix[i][j];
+                }
+                if (i + j > n - 1) {
+                    raz = raz + (-1) * matrix[i][j];
                 }
 
             }
 
         }
+        System.out.println("Произведение диагоналей: " + diag1 * diag2);
+        System.out.println("Сумма элементов матрицы выше диагонали: " + sum);
         System.out.println("Разность элементов матрицы ниже диагонали: " + raz);
     }
 
